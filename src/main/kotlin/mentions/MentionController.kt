58dd46409015e0b58cb9d09701d39a79df2d9678
Diego Rocha <diegopolovinsky@gmail.com>
2019-09-20 18:35:08 -0300

    add: purple-mentions non-blocking api
    add: task config
    
    add: get method
    
    add: java config
    
    fix: jar config
    
    refactor: controller non-blocking
    
    refactor: logs

--- src/main/kotlin/mentions/MentionController.kt
+++ src/main/kotlin/mentions/MentionController.kt
@@ -2,13 +2,25 @@ package mentions
 
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
+import io.reactivex.Maybe
+import org.slf4j.LoggerFactory
 
 @Controller("/mentions")
 class MentionController {
 
-	@Post("/")
-	fun create(@Body body: String) {
+	private val log = LoggerFactory.getLogger(MentionController::class.java)
 
+	@Get("/")
+	fun get(): Maybe<String> {
+		log.info("get mentions")
+		return Maybe.just("success")
+	}
+
+	@Post("/")
+	fun create(@Body body: String): Maybe<String> {
+		log.info("receive mention payload={}", body)
+		return Maybe.just("created")
 	}
 }
